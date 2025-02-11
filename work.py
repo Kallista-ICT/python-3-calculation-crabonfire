6a4e0b3638212bdafa1f7a11fd90de7738ba9106
crabonfire <kepitingkebakaran@gmail.com>
2025-02-11 07:28:42 +0000

    Submit assignment ##

---
+++ work.py
@@ -0,0 +1,10 @@
+#the code the program will follow when given distance and time
+distance = float(input("Enter the distance (in meters): "))
+time = float(input("Enter the time (in seconds): "))
+
+#the equation the program will follow 
+velocity = distance / time
+
+#the result/velocity 
+print("The velocity is {velocity:.2f} m/s.")
+
